e1a36c3366214142727843e8b9f8991a4503787e
Baruch Rotbart <baruchrotbart@Baruchs-MacBook-Pro.local>
2021-11-28 15:44:54 -0600

    hw

--- app/blueprints/api/routes.py
+++ app/blueprints/api/routes.py
@@ -1,4 +1,5 @@
 from . import bp as api
+from app import db
 from app.blueprints.auth.models import User
 from app.blueprints.blog.models import Post
 from flask import jsonify, request
@@ -10,6 +11,8 @@ from .auth import basic_auth
 @api.route('/token', methods=['POST'])
 @basic_auth.login_required
 def get_token():
+    print('GET TOKEN FUNCTION')
+    
     token = basic_auth.current_user().get_token()
     return jsonify({'token': token})
 
@@ -76,46 +79,54 @@ def delete_user(id):
 # Blog Post routes
 
 @api.route('/posts')
-def get_posts():
+def get_users():
+    """
+    [GET] /api/posts - Returns all posts
+    """
     posts = Post.query.all()
-    return jsonify([p.to_dict() for p in posts])
-
+    return jsonify([post.to_dict() for post in posts])
 
 @api.route('/posts/<id>')
-def get_post(id):
+def get_user(id):
     """
-    [GET] /api/posts/<id> - Return the post of url id
+    [GET] /api/posts/<id> - Return posts based on id
     """
     post = Post.query.get_or_404(id)
     return jsonify(post.to_dict())
 
-
 @api.route('/posts', methods=['POST'])
-def create_post():
+def create_posts():
     data = request.json
-    for field in ['title', 'content', 'user_id']:
+    for field in ['title', 'content']:
         if field not in data:
             return jsonify({'error': f'You are missing the {field} field'}), 400
+    # Grab data from the request body
     title = data['title']
     content = data['content']
-    user_id = data['user_id']
 
-    new_post = Post(title, content, user_id)
+    # Check if the title from the already exists in the Post table
+    existing_post = Post.query.filter_by(title=title).all()
+    # If there is a post that has a Title taken, message them asking them to try again
+    if existing_post:
+        return jsonify({'error': f'The {title} of your post has been used already. Create a new Title.'}), 400
+
+    # Create new post
+    new_post = Post(title, content)
     new_post.save()
-    return jsonify(new_post.to_dict()), 201
 
+    return jsonify(new_post.to_dict())
 
 @api.route('/posts/<id>', methods=['PUT'])
 def update_post(id):
-    post_to_update = Post.query.get_or_404(id)
-    update_data = request.json
-    post_to_update.update_post(update_data)
-    return jsonify(post_to_update.to_dict())
-
+    post = Post.query.get_or_404(id)
+    data = request.json
+    post.update_post(data)
+    return jsonify(post.to_dict())
 
 @api.route('/posts/<id>', methods=['DELETE'])
 def delete_post(id):
-    post_to_delete = Post.query.get_or_404(id)
-    post_to_delete.delete()
-    return jsonify({}), 204
-
+    post = Post.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    
+    return "Post was successfully deleted"
